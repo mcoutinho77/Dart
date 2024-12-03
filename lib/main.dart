66991e75406b7f8e2fed987457c395f3c246a491
mcoutinho77 <matheus.breno2023@outlook.com>
2024-12-03 19:09:16 -0300

    segundo do commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ void main() {
 }
 
 class Login extends StatefulWidget {
-  const Login({super.key});
+  const Login({Key? key}) : super(key: key);
 
   @override
   State<Login> createState() => _LoginState();
@@ -20,36 +20,110 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 9, 112, 196),
-          title: const Text(
-            "Tela de Login",
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 9, 112, 196),
+        title: const Text(
+          "Tela de Login",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+          child: Form(
+            child: Column(
+              children: [
+                const Icon(
+                  Icons.account_circle_rounded,
+                  color: Colors.white,
+                  size: 110,
+                ),
+                const SizedBox(height: 30),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "E-mail",
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w200,
+                      fontSize: 16,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w200,
+                    fontSize: 16,
+                  ),
+                ),
+                const SizedBox(height: 5),
+                TextFormField(
+                  decoration: InputDecoration(
+                    hintText: "Senha",
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintStyle: const TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.w200,
+                      fontSize: 16,
+                    ),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.w200,
+                    fontSize: 16,
+                  ),
+                  obscureText: true,
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 15, bottom: 15),
+                  child: SizedBox(
+                    height: 35,
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor:
+                              const Color.fromARGB(255, 9, 112, 196),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6),
+                          )),
+                      child: const Text(
+                        "Entrar",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Cadastre-se",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
-        body: Form(
-          child: Column(
-            children: [
-              const Icon(
-                Icons.account_circle_rounded,
-              color: Colors.white,
-              size: 110,
-              ),
-              TextFormField(
-                decoration:const InputDecoration(
-                hintText: "E-mail",
-                fillColor: Colors.white,
-              )
-            ),
-          ],
-        ),
-      )
+      ),
     );
   }
 }
